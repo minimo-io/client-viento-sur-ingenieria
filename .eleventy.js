0ea1dca89a289f8c26fc62c1e4d0d7be225e8a68
minimo-io <nicolas@minimo.io>
2024-08-21 14:52:05 -0300

    Uploads folder

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function (eleventyConfig) {
 
     // Copy Image Folder to /_site
     eleventyConfig.addPassthroughCopy("./src/static/img");
+    eleventyConfig.addPassthroughCopy("./src/static/uploads");
 
     // Copy favicon to route of /_site
     eleventyConfig.addPassthroughCopy("./src/favicon.ico");
